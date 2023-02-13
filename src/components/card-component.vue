b89552bdcf4ee5ff3cec6a389c368441728335cc
ValentinSaraiva-MMI <95092193+ValentinSaraiva-MMI@users.noreply.github.com>
2023-02-13 15:05:04 +0100

    first commit

--- src/components/card-component.vue
+++ src/components/card-component.vue
@@ -1,13 +1,12 @@
 <template>
-  <!--h-[156px] w-[175px] md:h-[184px] md:w-[207px] lg:h-[246px] lg:w-[260px] xl:h-[308px] xl:w-[325px]-->
+<div class="flex justify-center mt-12 mb-2">
 
-  <div class="relative h-[280px] w-[260px] md:ml-5 md:h-[246px] md:w-[260px] lg:h-[308px] lg:w-[325px]">
-    <img :src="image" class="absolute left-[-1px] top-[-1px] w-auto sm:h-[115px] md:h-[144px] lg:h-[180px]" alt="image logiciel" />
-
-    <p class="absolute right-20 top-48 w-[325px] text-center font-lato text-[28px] text-white sm:-mt-14 md:-mt-7 xl:mt-1 xl:mb-1">
-      {{ nom }}
-    </p>
+  <div class="relative w-max">
+    <img :src="image" class=" w-auto sm:h-[115px] md:h-[144px] lg:h-[180px]" alt="image logiciel" />
+ 
   </div>
+    </div>
+
 </template>
 
 <script>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,4 +1,6 @@
 <template>
+
+
   <div class="flex h-16 w-full items-center justify-between p-7">
     <div class="flex h-12 w-32 place-items-center justify-between p-1"></div>
     <ul class="x-justify-center z-50 hidden flex-auto gap-2 sm:flex">
@@ -10,11 +12,11 @@
       <li class="hover-underline-animation mx-2 flex flex-auto font-lato text-base text-white">
         <router-link to="/apropos">À propos</router-link>
       </li>
-      <li class="hover-underline-animation mx-2 flex flex-auto font-lato text-base text-white">
+      <li   class="hover-underline-animation mx-2 flex flex-auto font-lato text-base text-white">
         <router-link to="/">Projets</router-link>
       </li>
 
-      <li class="hover-underline-animation mx-2 flex flex-auto font-lato text-base text-white">
+      <li   class="hover-underline-animation mx-2 flex flex-auto font-lato text-base text-white">
         <a href="../public/document/CV.pdf" download>CV</a>
       </li>
 
@@ -30,7 +32,14 @@
       :aria-expanded="menuOuvert"
       @click="menuOuvert = !menuOuvert"
     >
-      <menu-hamburger-portfolio class="flex w-7" />
+
+  <div class="flex relative z-50 md:hidden w-8 h-5
+   flex-col justify-between" @click="switchMenu">
+    <span class="w-full h-[3px] bg-white transition-all" :class="menuOuvert && `rotate-45`"></span>
+    <span class="w-2/3 h-[2px] bg-white self-end transition-all" :class="menuOuvert && ` w-[100%]  hidden absolute `"></span>
+        <span class="w-full h-[2px] bg-white transition-all" :class="menuOuvert && ` absolute -rotate-45 `"></span>
+  </div>
+
     </button>
 
     <div
@@ -38,23 +47,29 @@
       class="fixed inset-0 z-100 translate-x-full overflow-y-hidden bg-noir motion-safe:transition-transform motion-safe:duration-1000"
       :class="{ 'translate-x-0': menuOuvert }"
     >
-      <div class="mt-7">
+      <div class="mt-11">
         <p class="ml-4 inline-block pr-3 font-montserrat text-4xl text-white">Portfolio</p>
         <p class="ml-4 inline-block font-montserrat text-2xl text-white">Valentin Saraiva</p>
       </div>
 
-      <ul class="ml-4 flex h-screen flex-col justify-evenly font-montserrat text-5xl text-white">
-        <li class=""><router-link to="/">Accueil </router-link></li>
-        <li class=""><router-link to="/apropos">À propos </router-link></li>
-        <li class=""><router-link to="/">Projets </router-link></li>
-        <li class=""><router-link to="/contact">Contact</router-link></li>
+      <ul   class="ml-4 flex h-5/6 -mt-6  flex-col justify-evenly font-montserrat text-4xl text-white">
+        <li    @click="menuOuvert = !menuOuvert" class=""><router-link to="/">Accueil </router-link></li>
+        <li    @click="menuOuvert = !menuOuvert" class=""><router-link to="/apropos">À propos </router-link></li>
+        <li   @click="menuOuvert = !menuOuvert"  class=""><router-link to="/">Projets </router-link></li>
+        <li    @click="menuOuvert = !menuOuvert" class=""><router-link to="/contact">Contact</router-link></li>
       </ul>
     </div>
 
     <!-- "z-50" place le menu eu dessus de tous les autes éléments.
             "relative" est indispensable avec un z- sinon ce dernier n'est pas pris en compte
             "sm:hidden permet de cacher l'élément lorsque l'écran est moins large que 640px    -->
+
+
+        
   </div>
+
+
+
 </template>
 
 <script>
